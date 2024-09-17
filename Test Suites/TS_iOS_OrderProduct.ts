6f9f01dea3328e821f5ffd2c697ea6dcdafaac16
maryovos <maryosina16@gmail.com>
2024-09-18 05:22:27 +0700

    update commit

---
+++ Test Suites/TS_iOS_OrderProduct.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_iOS_OrderProduct</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>0cd4840d-b2d3-4c03-81a2-e13cddee431e</testSuiteGuid>
+   <testCaseLink>
+      <guid>3df51736-f7c3-4f80-a961-6dd7bba0365a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_OrderProduct</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
