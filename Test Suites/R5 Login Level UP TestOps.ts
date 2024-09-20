c402f458245eccf8727f528ff0302bc92eb1d2e2
maryovos <maryosina16@gmail.com>
2024-09-20 09:40:50 +0700

    update commit

--- Test Suites/R5 Login Level UP TestOps.ts
+++ Test Suites/R5 Login Level UP TestOps.ts
@@ -12,10 +12,22 @@
    <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>046d01e8-4315-401a-ad4d-84a30542dfaf</testSuiteGuid>
    <testCaseLink>
-      <guid>a39b37ef-9b08-4500-bbbe-ec5c0659174a</guid>
+      <guid>7a57b195-7293-4272-80d1-31a30ee0848d</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/TestRail/TC Login</testCaseId>
+      <testCaseId>Test Cases/Mobile/Level UP/TC_Login_TestOps</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>abc6d9fa-63fe-4872-b64d-04e780313a35</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>24535526-02ea-4418-9c38-9cf78f714e8c</variableId>
+      </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
